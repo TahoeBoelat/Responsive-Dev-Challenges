d609ae8c96c312153b6c2d2b31fab8c5b5527504
Muhamad Nur Alfi Syahrin <30311659+TahoeBoelat@users.noreply.github.com>
2021-02-26 11:12:48 +0700

    Update index.js
    Adding DOM syntax for ammount item form

--- checkoutPage/index.js
+++ checkoutPage/index.js
@@ -1,1 +1,15 @@
+const MinusButton = document.getElementById('buttonMinus');
+const PlusButton = document.getElementById('buttonAdd');
+const InputFile = document.getElementById('inputAmmount');
 
+MinusButton.addEventListener('click', event => {
+    event.preventDefault();
+    const currentValue = Number(InputFile.value) || 0;
+    InputFile.value = currentValue - 1;
+});
+
+PlusButton.addEventListener('click', event => {
+    event.preventDefault();
+    const currentValue = Number(InputFile.value) || 0;
+    InputFile.value = currentValue + 1;
+});
